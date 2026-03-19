a101a9c562d2854b53719587bef9bffceada10c6
Hector Perez <hectorphone@gmail.com>
2026-03-19 11:10:25 +0100

    Script interactivo de Python

---
+++ tareas.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cómo te llamas?")
+edad = int(input("Cuantos años tienes?"))
+
+if edad >= 18:
+    print(f"{nombre}, eres mayor de edad")
+else:
+    print(f"{nombre}, eres menor de edad")
