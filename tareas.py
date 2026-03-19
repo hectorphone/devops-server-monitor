57536b88b8a1b0c0ff9bd0af2d42949c99732162
Hector Perez <hectorphone@gmail.com>
2026-03-19 11:31:13 +0100

    Seguimos con las pruebas de nuevos script

--- tareas.py
+++ tareas.py
@@ -1,3 +1,4 @@
+import os
 nombre = input("¿Cómo te llamas?")
 edad = int(input("Cuantos años tienes?"))
 
@@ -5,3 +6,13 @@ if edad >= 18:
     print(f"{nombre}, eres mayor de edad")
 else:
     print(f"{nombre}, eres menor de edad")
+
+print("----SISTEMA----")
+print("Usuario")
+os.system("whoami")
+
+print("Ruta actual")
+os.system("pwd")
+
+print("Archivos")
+os.system("ls")
