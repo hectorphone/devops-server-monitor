637e4fb654c5ae8af926c0ddab12a97757e1bec4
Hector Perez <hectorphone@gmail.com>
2026-03-23 12:09:22 +0100

    crear carpeta y escribir archivo log

--- automatizacion.py
+++ automatizacion.py
@@ -3,7 +3,16 @@ import os
 carpeta = input("Nombre de la carpeta: ")
 
 if os.path.exists(carpeta):
-    print("Carpeta ya existe")
+    print("La carpeta ya existe")
 else:
     os.mkdir(carpeta)
     print("Carpeta creada")
+    
+ruta = carpeta + "/log.txt"
+print(ruta)
+
+archivo = open(ruta, "w", encoding="utf-8")
+archivo.write("Hola\nAdios")
+archivo.close()
+
+print("Texto añadido al archivo")
